0e94995ffa7bd15458b058e2026b0c1d30863a87
jazzjohon <samueljazzjohn@gmail.com>
2024-07-03 02:43:50 +0530

    fix: removed unnecessary codes

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -5,34 +5,12 @@ interface AppConfig {
   app: {
     port: number;
   };
-  mongoDB: {
-    uri: string;
-  };
-  facebook:{
-    appId: string;
-    appSecret: string;
-    authUrl: string
-  },
-  secret:{
-    key: string
-  }
 }
 
 const config: AppConfig = {
   app: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 5000,
   },
-  mongoDB: {
-    uri: process.env.MONGODB_URI || '',
-  },
-  facebook: {
-    appId: process.env.FACEBOOK_APP_ID || '',
-    appSecret: process.env.FACEBOOK_APP_SECRET || '',
-    authUrl: process.env.FACEBOOK_AUTH_URL || ''
-  },
-  secret: {
-    key: process.env.SECRET_API_KEY || ''
-  }
 };
 
 export default config;
